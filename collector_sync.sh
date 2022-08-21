807b27397d3200f6c4d13f3cc46cb36fa5a90307
Vaughan Jones <vlj7491@gmail.com>
2022-08-21 17:07:58 +0200

    .

--- collector_sync.sh
+++ collector_sync.sh
@@ -2,10 +2,19 @@
 
 if [[ $1 == "--config" ]]; then
   cat <<EOF
-    configVersion: v1
-    schedule:
-    - crontab: "* * * * *"
-      allowFailure: true
+{
+  "configVersion":"v1",
+  "schedule": [
+    {
+      "name": "every 1 min",
+      "crontab": "*/1 * * * *"
+    },
+    {
+      "name": "every 5 min",
+      "crontab": "*/5 * * * *"
+    }
+  ]
+}
 EOF
 else
   echo "schedule"
